829fe4275b4049fe0fcead98301b2fb41ceedd7e
Matheus <joaoomatheus@hotmail.com>
2021-03-30 09:25:16 -0300

    Finalized project

---
+++ app.js
@@ -0,0 +1,52 @@
+let express = require("express");
+let app = express();
+let products = [];
+
+//by writing this we convert the product referente to an
+// json object
+app.use(express.static("public"));
+app.use(express.json());
+let id = 1;
+
+app.get("/products", (req, resp) => {
+  resp.send(products);
+});
+
+app.post("/products", (req, resp) => {
+  let newProduct = req.body;
+  newProduct.id = id;
+  id++;
+  products.push(newProduct);
+  resp.send("Created");
+});
+
+app.put("/products/:id", (req, resp) => {
+  let productId = +req.params.id;
+  let productForUpdate;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == productId) {
+      productForUpdate = products[i];
+      break;
+    }
+  }
+  if (productForUpdate) {
+    productForUpdate.price = req.body.price;
+    resp.send("Updated!");
+  } else {
+    resp.send("Rejected!");
+  }
+});
+
+app.delete("/products/:id", (req, resp) => {
+  let productId = +req.params.id;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == productId) {
+      products.splice(i, 1);
+      resp.send("Deleted!");
+      return;
+    }
+  }
+  resp.send("Rejected");
+});
+
+app.listen(3000, () => console.log("Listeing 3000!"));
